a931cf857b400e301741714c7b5fd2a5398e5e48
thucop <thucop@gmail.com>
2017-08-22 08:56:21 +0800

    [feature] postman by thucop

---
+++ app/views/admin/postman.php
@@ -0,0 +1,33 @@
+<script src="/public/js/jquery.jqote2.min.js"></script>
+<script src="/public/js/postman.adm.js"></script>
+<section id="sec-postman">
+  <div class="container">
+    <?php if ($_SESSION['admin']) : ?>
+      <div class="container">
+        <div style="width:95%;">
+          致：<select id="email-rcptto">
+                <option value='1'>所有單位人員</option>
+                <option value='2'>所有單位主管</option>
+                <option value='3'>所有單位職員</option>
+<!--
+                <option value='4'>所有未填報單位聯絡人</option>
+-->
+              </select>
+        </div>
+        <div style="width:95%;">
+          <label>信件主旨</label>
+          <input type="text" id="email-subject" style="width:95%"/>
+        </div>
+        <div style="width:95%;">
+          <div id="email-body" style="width:95%;height:300px;">
+          <label>信件內容</label>
+          <textarea style="width:100%;height:260px;"></textarea>
+        </div>
+        <div style="width:95%;text-align:center">
+          <button id="email-send" orgtxt="發送信件">發送信件</button>
+        </div>
+      </div>
+    <?php endif;?>
+  </div>
+</section>
+
