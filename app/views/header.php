9c38cccc6c4ab65d91bd556d7c626644277568bc
thucop <thucop@gmail.com>
2017-11-14 15:16:45 +0800

    [bugfix] maintain

--- app/views/header.php
+++ app/views/header.php
@@ -131,7 +131,7 @@
       }
     };
   </script>
-  <script src="/public/js/mojo.combo.v11.10.js"></script>
+  <script src="/public/js/mojo.combo.v11.14.js"></script>
   <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body data-mojo="<?php echo (isset($_SESSION['admin'])? $_SESSION['admin']['session'] : (isset($_SESSION['agent'])? $_SESSION['agent']['session'] : ''));?>" data-error="<?php echo (isset($error_code)? $error_code : '') ;?>">
