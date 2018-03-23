8d3695760a6867eb70075d0d8227ab1b0f2ebed0
thucop <thucop@gmail.com>
2018-03-23 09:49:49 +0800

    update reset password recaptcha

--- app/controllers/EmailController.class.php
+++ app/controllers/EmailController.class.php
@@ -9,6 +9,7 @@ class EmailController extends Controller {
     }
 
     function contents( $timestamp, $session, $username, $email ) {
+        /*
         $html = 
         '<section id="sec-activate">
             <form method="POST" id="form-activate" class="form-horizontal">
@@ -38,7 +39,39 @@ class EmailController extends Controller {
                 </div>
             </form>
         </section>';
+        */
+        $html =
+        '<section id="sec-activate">
+            <form method="POST" id="form-activate" class="form-horizontal">
+                <div class="form-group col-xs-12">
+                    <h3>帳號 : '. $username .'</h3>
+                    <h3>信箱 : '. $email .'</h3>
+                    <input type="hidden" id="username" name="username" class="form-control" value="'. $username .'"/>
+                    <input type="hidden" id="email" name="email" class="form-control" value="'. $email .'"/>
+                    <input type="hidden" id="timestamp" name="timestamp" class="form-control" value="'. $timestamp .'"/>
+                    <input type="hidden" id="session" name="session" class="form-control" value="'. $session .'"/>
+                </div>
+                <div class="form-group col-xs-12">
+                    <input type="text" id="userpass" name="userpass" placeholder="請輸入您的密碼" class="form-control" />
+                    <i id="icon-userpass" class="btn fa fa-eye"></i>
+                </div>
+                <div class="form-group col-xs-12">
+                    <input type="text" id="checkpass" name="checkpass" placeholder="請再次輸入您的密碼" class="form-control" />
+                    <i id="icon-checkpss" class="btn fa fa-eye"></i>
+                </div>
+                <div class="form-group col-xs-12">
+                    <div id="recaptcha" class="g-recaptcha" data-sitekey="6LdLuCMUAAAAANHGd41Qo7Mo2jGT_xFD3iALDo1O" data-callback="mojocallback" data-size="invisible"></div>
+                </div>
+                <div class="form-group col-xs-12">
+                    <div class="col-xs-6">
+                        <a href="/email/activate" class="btn btn-lg btn-block btn-primary" id="btn-activate-agent" >重設密碼</a>
+                    </div>
+                </div>
+            </form>
+        </section>';
+
         return $html;
+
     }
 
     function headers() {
